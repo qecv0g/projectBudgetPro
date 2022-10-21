ce21d66bfd2850969f7a4ec6d104ead04a074c6a
HibestMekonnen <116267513+HibestMekonnen@users.noreply.github.com>
2022-10-21 18:54:17 +0200

    Revert "app for user authentication and views"
    This reverts commit 170982c38e2fd041c7ab3e8f56390e3ecaa985f2.

--- budgetpro/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . views import RegisterView, RetriveUserView
-urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('me', RetriveUserView.as_view()),
-]
